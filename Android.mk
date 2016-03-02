529e1422b381dede24549b18497f7401da9551a0
linwei <linweigjj@gmail.com>
2016-03-02 10:51:27 +0800

    udpate android_iconv

---
+++ Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libiconv
+LOCAL_CFLAGS := \
+   -Wno-multichar \
+   -D_ANDROID \
+   -DLIBDIR="c" \
+   -DBUILDING_LIBICONV \
+   -DIN_LIBRARY
+ 
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH) \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/lib \
+	$(LOCAL_PATH)/libcharset/include
+     
+LOCAL_SRC_FILES := \
+	libcharset/lib/localcharset.c \
+   	lib/iconv.c \
+   	lib/relocatable.c
+include $(BUILD_STATIC_LIBRARY)
